3f1af8b6eda3231da07a009f3dccfcd0f9ae1bfb
Allen, Timothy <tallen@wharton.upenn.edu>
2018-09-04 14:25:57 +0000

    Refactor to use a method which can be overridden.

--- shapeshifter/mixins.py
+++ shapeshifter/mixins.py
@@ -7,9 +7,12 @@ class MultiSuccessMessageMixin(object):
     """
     success_message = None
 
+    def get_success_message(self):
+        return self.success_message
+
     def forms_valid(self):
         response = super(MultiSuccessMessageMixin, self).forms_valid()
-        if self.success_message:
+        if self.get_success_message():
             messages.success(self.request, self.success_message)
 
         return response
